68bebf3cda9c2fe25cfef3c2bf9e92b98def30eb
René Meye <rene@meye.md>
2013-10-27 04:26:45 +0100

    Appointmentspaht

--- app/controllers/token_authentications_controller.rb
+++ app/controllers/token_authentications_controller.rb
@@ -3,14 +3,14 @@ class TokenAuthenticationsController < ApplicationController
   def create
     @user = AdminUser.find(params[:admin_user_id])
     @user.reset_authentication_token!
-    redirect_to index_appointments_path
+    redirect_to appointments_path
   end
 
   def destroy
     @user = AdminUser.find(params[:id])
     @user.authentication_token = nil
     @user.save
-    redirect_to index_appointments_path
+    redirect_to appointments_path
   end
 
 end
