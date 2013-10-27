312dc8e3075f4d14682f471b4d53ebdf4adaed93
René Meye <rene@meye.md>
2013-10-27 03:09:42 +0100

    serviceURL dynamic

--- app/admin/appointment.rb
+++ app/admin/appointment.rb
@@ -13,7 +13,7 @@ ActiveAdmin.register Appointment do
   member_action :push, :method => :get do
     @appointment = Appointment.find(params[:id])
 
-    options = { :body => {:phoneNumber => @appointment.patient.tel_number, :message => "Ihr Termin bei #{@appointment.medic.title} #{@appointment.medic.name} wurde geändert."[0,50], :serviceURL => "https://rawgithub.com/DoctorPush/doctor/master/appointment_test.json"}}
+    options = { :body => {:phoneNumber => @appointment.patient.tel_number, :message => "Ihr Termin bei #{@appointment.medic.title} #{@appointment.medic.name} wurde geändert."[0,50], :serviceURL => appointment_url(@appointment, format: :json)}}
     response = HTTParty.post("http://pushdoc.delphinus.uberspace.de/api/message", options)
 
     redirect_to({action: :show, id: params[:id]})
@@ -42,7 +42,7 @@ ActiveAdmin.register Appointment do
 
   controller do
     def permitted_params
-      params.permit appointment: [:patient_id, :medic_id, :start, :end, :start_date, :start_time_hour, :start_time_minute, :end_date, :end_time_hour, :end_time_minute, :title, :created_at, :updated_at]
+      params.permit appointment: [:patient_id, :medic_id, :start, :end, :start_date, :start_time_hour, :start_time_minute, :end_date, :end_time_hour, :end_time_minute, :title, :created_at, : updated_at]
     end
 
     alias_method :update_appointment, :update
